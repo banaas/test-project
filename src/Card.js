d1536ef9ac0b0817b4fca48ce482ead7a3ec0d58
banaas <abdurahimbana@gmail.com>
2022-08-22 12:25:53 -0400

    Added new files

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { robots } from './robots';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
     return (
-        <div className='bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={'https://robohash.org/${this.props.id}?200x200'} alt='photo'/>
+        <div className='tc bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={'https://robohash.org/${id}?200x200'} alt='photo'/>
             <div>
-                <h2>props.name</h2>
-                <p>props.email</p>
+                <h2>{ name }</h2>
+                <p>{ email }</p>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './robots';
@@ -10,7 +10,7 @@ import { robots } from './robots';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App />
   </React.StrictMode>
 );
 
